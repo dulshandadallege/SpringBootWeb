7c76f749006f547be3bed16fbcb9ea3142db722f
dulshandadallege <dulshandadallege@gmail.com>
2020-03-02 20:35:15 +0530

    first commit

---
+++ src/main/java/com/dulshan/springboot/web/springbootfirstwebapplication/SpringBootFirstWebApplication.java
@@ -0,0 +1,14 @@
+package com.dulshan.springboot.web.springbootfirstwebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstWebApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dulshan/springboot/web/springbootfirstwebapplication/SpringBootFirstWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dulshan.springboot.web.springbootfirstwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
